248ddd152a7282b9a31b5dfaa7fa8c95beefc6c7
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-08 11:52:02 +0100

    create file to load player

---
+++ projects/vm/srcs/dump/load_player.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_player.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/08 11:48:50 by fbenneto          #+#    #+#             */
+/*   Updated: 2019/03/08 11:50:08 by fbenneto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <dump.h>
+
+void		load_players(t_vm *vm)
+{
+	int i;
+
+	i = -1;
+	while (++i < vm->players_count)
+		load_player(vm, i);
+}
+
+void		load_player(t_vm *vm, int player_idx)
+{
+	void		*champ_bin;
+	uint32_t	champ_size;
+	void		*start_in_memory;
+
+	champ_bin = get_champ_bin(vm, player_idx);
+	champ_size = get_champ_size(vm, player_idx);
+	ft_printf("champ_size = %d\n", champ_size);
+	if (champ_size > (uint32_t)CHAMP_MAX_SIZE)
+		set_errno_exit(EFBIG, vm->players[player_idx].file_name);
+	start_in_memory = get_start_in_memory(vm, player_idx);
+	ft_memcpy(start_in_memory, champ_bin, champ_size);
+}
+
+void		*get_champ_bin(t_vm *vm, int player_idx)
+{
+	return (vm->players[player_idx].file_buffer + sizeof(header_t));
+}
+
+uint32_t	get_champ_size(t_vm *vm, int player_idx)
+{
+	return ((uint32_t)vm->players[player_idx].header.prog_size);
+}
+
+void		*get_start_in_memory(t_vm *vm, int player_idx)
+{
+	return (vm->memory + ((MEM_SIZE / vm->players_count) * player_idx));
+}
--- projects/vm/srcs/vm.c
+++ projects/vm/srcs/vm.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/01 11:42:01 by glodi             #+#    #+#             */
-/*   Updated: 2019/03/07 18:43:37 by glodi            ###   ########.fr       */
+/*   Updated: 2019/03/08 11:48:34 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,43 +24,3 @@ int main(int argc, char *argv[])
 	// print_dump(vm.memory);
 	return (0);
 }
-
-void		load_players(t_vm *vm)
-{
-	int i;
-
-	i = -1;
-	while (++i < vm->players_count)
-		load_player(vm, i);
-}
-
-void		load_player(t_vm *vm, int player_idx)
-{
-	void		*champ_bin;
-	uint32_t	champ_size;
-	void		*start_in_memory;
-
-	champ_bin = get_champ_bin(vm, player_idx);
-	champ_size = get_champ_size(vm, player_idx);
-	ft_printf("champ_size = %d\n", champ_size);
-	if (champ_size > (uint32_t)CHAMP_MAX_SIZE)
-		set_errno_exit(EFBIG, vm->players[player_idx].file_name);
-	start_in_memory = get_start_in_memory(vm, player_idx);
-	ft_memcpy(start_in_memory, champ_bin, champ_size);
-}
-
-void		*get_champ_bin(t_vm *vm, int player_idx)
-{
-	return (vm->players[player_idx].file_buffer + sizeof(header_t));
-}
-
-uint32_t	get_champ_size(t_vm *vm, int player_idx)
-{
-	return ((uint32_t)vm->players[player_idx].header.prog_size);
-}
-
-void		*get_start_in_memory(t_vm *vm, int player_idx)
-{
-	return (vm->memory + ((MEM_SIZE / vm->players_count) * player_idx));
-}
-
