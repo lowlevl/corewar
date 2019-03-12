fbfa6820bf29514a7ce8a162dacbe1ce4bb28b20
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-12 16:34:24 +0100

    write utils files

--- projects/vm/srcs/opcode/get.u.c
+++ projects/vm/srcs/opcode/get.u.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils.c                                            :+:      :+:    :+:   */
+/*   get.u.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 13:52:54 by glodi             #+#    #+#             */
-/*   Updated: 2019/03/12 16:17:23 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/12 16:25:01 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,46 +24,9 @@ int get_type_arg(uint8_t co, uint8_t arg_number)
 	return -1;
 }
 
-uint16_t read_arg(t_process *process, uint8_t *mem, int type)
-{
-	uint16_t val;
-	size_t idx;
-
-	idx = get_idx_in_memory(process);
-	if (type == T_REG)
-	{
-		process_move_cursor(process, 1);
-		return mem[idx];
-	}
-	else if (type == T_IND)
-	{
-		val = bswap_16(*(uint16_t *)(mem + idx));
-		process_move_cursor(process, sizeof(val));
-		return val;
-	}
-	else if (type == T_DIR)
-	{
-		val = bswap_16(*(uint16_t *)(mem + idx));
-		process_move_cursor(process, sizeof(val));
-		return val;
-	}
-	return -1;
-}
-
 uint32_t get_reg(t_process *process, uint8_t reg_idx)
 {
 	if (reg_idx < 1 || reg_idx >= REG_SIZE)
 		return -1;
 	return process->regs[reg_idx - 1];
-}
-
-uint8_t  read_octet_code(t_process *process, uint8_t *mem)
-{
-	uint8_t octect_code;
-	size_t	idx;
-
-	idx = get_idx_in_memory(process);
-	octect_code = mem[idx];
-	process_move_cursor(process, 1);
-	return octect_code;
 }
---
+++ projects/vm/srcs/opcode/write.u.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write.u.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/03/12 16:26:13 by fbenneto          #+#    #+#             */
+/*   Updated: 2019/03/12 16:34:01 by fbenneto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "opcode.h"
+
+static size_t get_index(size_t i)
+{
+	return i % MEM_SIZE;
+}
+
+void write_in_memory(uint8_t *mem, uint8_t *content, size_t len, size_t at)
+{
+	size_t i;
+
+	i = 0;
+	while (i < len)
+	{
+		mem[get_index(at + i)] = content[i];
+		i++;
+	}
+}
