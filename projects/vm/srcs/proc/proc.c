0d003d3735db06c52fb1b1ddba86abcf027f53af
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-12 14:14:10 +0100

    use g_ops for exec opcode

--- projects/vm/srcs/proc/proc.c
+++ projects/vm/srcs/proc/proc.c
@@ -6,11 +6,12 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/06 16:45:42 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/12 12:15:58 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/12 14:10:43 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "g_op.h"
+#include "g_ops.h"
 #include <proc.h>
 
 void init_processes(t_vm *vm)
@@ -92,6 +93,7 @@ void exec_process(t_vm *vm, t_process *process)
 {
 	uint8_t *   pos;
 	const t_op *op;
+	const t_ops *ops;
 
 	pos = get_pos_in_memory(vm->memory, process);
 	op = get_opcode(*pos);
@@ -99,11 +101,12 @@ void exec_process(t_vm *vm, t_process *process)
 		ft_printf("NOPE\n");
 	ft_printf("Exec process %p %#x (%s)\n", pos - vm->memory, op->opcode,
 			  op->name);
-	if (op->opcode == 0x0b) // sti
+	ops = g_ops + op->opcode - 1;
+	if (ops->name != NULL)
 	{
-		// if (process->exec_cycle == -1)
-		// 	process->exec_cycle = vm->cycle_count + op->duration;
-		// if (process->exec_cycle == vm->cycle_count)
-			exec_sti(vm, process, op);
+		if (process->exec_cycle == -1)
+			process->exec_cycle = vm->cycle_count + op->duration;
+		if (process->exec_cycle == vm->cycle_count)
+			ops->f(vm, process, op);
 	}
 }
