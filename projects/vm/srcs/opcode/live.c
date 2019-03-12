0c29c6dae6881b18d230b38c9f13c5704f7c1ad6
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-12 17:03:25 +0100

    wip live function

--- projects/vm/srcs/opcode/live.c
+++ projects/vm/srcs/opcode/live.c
@@ -1,39 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   g_ops.h                                            :+:      :+:    :+:   */
+/*   live.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/12 14:01:04 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/12 17:02:39 by fbenneto         ###   ########.fr       */
+/*   Created: 2019/03/12 17:01:28 by fbenneto          #+#    #+#             */
+/*   Updated: 2019/03/12 17:02:03 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef G_OPS_H
-# define G_OPS_H
+#include "opcode.h"
 
-# include "opcode.h"
-# include "struct.h"
+void exec_live(t_vm *vm, t_process *process, const t_op *op)
+{
 
-const t_ops g_ops[17] = {
-	{ "live", exec_live },
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{ "sti", exec_sti },
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-	{NULL, NULL},
-};
-
-#endif // !G_OPS_H
+}
