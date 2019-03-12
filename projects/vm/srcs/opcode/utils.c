4f8f75d49d597999a695588a499cb6bfd69d3ebb
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-12 16:18:40 +0100

    use new val

--- projects/vm/srcs/opcode/utils.c
+++ projects/vm/srcs/opcode/utils.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 13:52:54 by glodi             #+#    #+#             */
-/*   Updated: 2019/03/12 13:29:19 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/12 16:17:23 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,26 +24,27 @@ int get_type_arg(uint8_t co, uint8_t arg_number)
 	return -1;
 }
 
-uint16_t read_arg(uint8_t **pos, int type)
+uint16_t read_arg(t_process *process, uint8_t *mem, int type)
 {
 	uint16_t val;
+	size_t idx;
 
+	idx = get_idx_in_memory(process);
 	if (type == T_REG)
 	{
-		val = **pos;
-		*pos = *pos + 1;
-		return val;
+		process_move_cursor(process, 1);
+		return mem[idx];
 	}
 	else if (type == T_IND)
 	{
-		val = bswap_16(**(uint16_t **)pos);
-		*pos = *pos + sizeof(val);
+		val = bswap_16(*(uint16_t *)(mem + idx));
+		process_move_cursor(process, sizeof(val));
 		return val;
 	}
 	else if (type == T_DIR)
 	{
-		val = bswap_16(**(uint16_t **)pos);
-		*pos = *pos + sizeof(val);
+		val = bswap_16(*(uint16_t *)(mem + idx));
+		process_move_cursor(process, sizeof(val));
 		return val;
 	}
 	return -1;
@@ -56,11 +57,13 @@ uint32_t get_reg(t_process *process, uint8_t reg_idx)
 	return process->regs[reg_idx - 1];
 }
 
-uint8_t read_octet_code(uint8_t **pos)
+uint8_t  read_octet_code(t_process *process, uint8_t *mem)
 {
 	uint8_t octect_code;
+	size_t	idx;
 
-	octect_code = **pos;
-	*pos = *pos + 1;
+	idx = get_idx_in_memory(process);
+	octect_code = mem[idx];
+	process_move_cursor(process, 1);
 	return octect_code;
 }
