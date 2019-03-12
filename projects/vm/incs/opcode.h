e4618234425dfb8179562cdbf4815ce90c07520e
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-12 16:49:19 +0100

    add func write

--- projects/vm/incs/opcode.h
+++ projects/vm/incs/opcode.h
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/11 13:28:31 by glodi             #+#    #+#             */
-/*   Updated: 2019/03/12 15:37:56 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/12 16:35:11 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@
 #include "dump.h"
 #include "libft.h"
 #include "libpf.h"
-#include "struct.h"
 #include "proc.h"
+#include "struct.h"
 
 /**
  * utils
@@ -27,6 +27,7 @@ int		 get_type_arg(uint8_t co, uint8_t arg_number);
 uint32_t get_reg(t_process *process, uint8_t reg_idx);
 uint16_t read_arg(t_process *process, uint8_t *mem, int type);
 uint8_t  read_octet_code(t_process *process, uint8_t *mem);
+void	 write_in_memory(uint8_t *mem, uint8_t *content, size_t len, size_t at);
 
 /**
  * command
