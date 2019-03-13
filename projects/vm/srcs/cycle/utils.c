caf8c67eb06abdf10ac5f177b4c2898d17820e5a
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-13 11:24:54 +0100

    print proc state

--- projects/vm/srcs/cycle/utils.c
+++ projects/vm/srcs/cycle/utils.c
@@ -6,34 +6,38 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 09:48:09 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/13 09:49:39 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/13 11:22:10 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+#include "cycle.h"
 #include "g_op.h"
 #include "g_ops.h"
-#include "cycle.h"
 
 void exec_process(t_vm *vm, t_process *process)
 {
-	uint8_t *   pos;
-	const t_op *op;
+	uint8_t *	pos;
+	const t_op * op;
 	const t_ops *ops;
 
 	pos = get_pos_in_memory(vm->memory, process);
 	op = get_opcode(*pos);
 	if (!op)
 		ft_printf("NOPE\n");
-	ft_printf("Exec process %p %#x (%s)\n", pos - vm->memory, op->opcode,
-			  op->name);
 	ops = g_ops + op->opcode - 1;
 	if (ops->name != NULL)
 	{
 		if (process->exec_cycle == -1)
 			process->exec_cycle = vm->cycle_count + op->duration;
-		if (process->exec_cycle == vm->cycle_count)
+		else if (process->exec_cycle == vm->cycle_count)
+		{
+			ft_printf("Exec process %p %#x (%s)\n", pos - vm->memory,
+				op->opcode, op->name);
 			ops->f(vm, process, op);
+		}
+		else
+			ft_printf("wait process %p %#x (%s)\n", pos - vm->memory,
+				op->opcode, op->name);
 	}
 }
 
