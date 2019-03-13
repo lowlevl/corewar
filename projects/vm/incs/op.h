39a47cbf46cfa6ea0e04ad1c56b859b326f5c06a
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-13 13:11:36 +0100

    add type and size for dir4

--- projects/vm/incs/op.h
+++ projects/vm/incs/op.h
@@ -6,12 +6,12 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/10/04 11:33:27 by zaz               #+#    #+#             */
-/*   Updated: 2019/03/12 10:30:48 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/13 13:04:42 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef OP_H
-# define OP_H
+#define OP_H
 
 /*
 ** Toutes les tailles sont en octets.
@@ -22,49 +22,49 @@
  * nb of bytes
  * do not change this macro
  */
-#define IND_SIZE				2
-#define REG_SIZE				4
-#define DIR_SIZE				REG_SIZE
+#define IND_SIZE 2
+#define REG_SIZE 4
+#define DIR_SIZE 4
+#define DIR_4_SIZE 8
 
+#define REG_CODE 1
+#define DIR_CODE 2
+#define IND_CODE 3
 
-# define REG_CODE				1
-# define DIR_CODE				2
-# define IND_CODE				3
+#define MAX_ARGS_NUMBER 4
+#define MAX_PLAYERS 4
+#define MEM_SIZE (4 * 1024)
+#define IDX_MOD (MEM_SIZE / 8)
+#define CHAMP_MAX_SIZE (MEM_SIZE / 6)
 
+#define COMMENT_CHAR '#'
+#define LABEL_CHAR ':'
+#define DIRECT_CHAR '%'
+#define SEPARATOR_CHAR ','
 
-#define MAX_ARGS_NUMBER			4
-#define MAX_PLAYERS				4
-#define MEM_SIZE				(4*1024)
-#define IDX_MOD					(MEM_SIZE / 8)
-#define CHAMP_MAX_SIZE			(MEM_SIZE / 6)
+#define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
 
-#define COMMENT_CHAR			'#'
-#define LABEL_CHAR				':'
-#define DIRECT_CHAR				'%'
-#define SEPARATOR_CHAR			','
+#define NAME_CMD_STRING ".name"
+#define COMMENT_CMD_STRING ".comment"
 
-#define LABEL_CHARS				"abcdefghijklmnopqrstuvwxyz_0123456789"
+#define REG_NUMBER 16
 
-#define NAME_CMD_STRING			".name"
-#define COMMENT_CMD_STRING		".comment"
-
-#define REG_NUMBER				16
-
-#define CYCLE_TO_DIE			1536
-#define CYCLE_DELTA				50
-#define NBR_LIVE				21
-#define MAX_CHECKS				10
+#define CYCLE_TO_DIE 1536
+#define CYCLE_DELTA 50
+#define NBR_LIVE 21
+#define MAX_CHECKS 10
 
 /*
 **
 */
 
-typedef char	t_arg_type;
+typedef char t_arg_type;
 
-#define T_REG					0x1
-#define T_DIR					0x2
-#define T_IND					0x4
-#define T_LAB					0x8
+#define T_REG 0x1
+#define T_DIR 0x2
+#define T_IND 0x4
+#define T_LAB 0x8
+#define T_DIR_4 0xf
 
 #define OC_REG 0b01
 #define OC_IND 0b10
@@ -74,16 +74,16 @@ typedef char	t_arg_type;
 **
 */
 
-# define PROG_NAME_LENGTH		(128)
-# define COMMENT_LENGTH			(2048)
-# define COREWAR_EXEC_MAGIC		0xea83f3
+#define PROG_NAME_LENGTH (128)
+#define COMMENT_LENGTH (2048)
+#define COREWAR_EXEC_MAGIC 0xea83f3
 
-typedef struct		header_s
+typedef struct header_s
 {
-  unsigned int		magic;
-  char				prog_name[PROG_NAME_LENGTH + 1];
-  unsigned int		prog_size;
-  char				comment[COMMENT_LENGTH + 1];
-}					header_t;
+	unsigned int magic;
+	char		 prog_name[PROG_NAME_LENGTH + 1];
+	unsigned int prog_size;
+	char		 comment[COMMENT_LENGTH + 1];
+} header_t;
 
 #endif
