84df111862e093622a4b9bb43661bec6b977eb7a
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-14 11:13:03 +0100

    delete line before print

--- projects/vm/srcs/vm/winner.c
+++ projects/vm/srcs/vm/winner.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 13:14:46 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/13 14:04:28 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/14 09:36:47 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void print_winner(t_vm *vm)
 	winner_id = vm->last_player_report_as_live;
 	players = vm->players;
 	i = 0;
-	ft_printf("cycle: %zu\n", vm->cycle_count);
+	ft_printf("\e[2Kcycle: %zu\n", vm->cycle_count);
 	while (i < vm->players_count)
 	{
 		if (players[i].id == winner_id)
