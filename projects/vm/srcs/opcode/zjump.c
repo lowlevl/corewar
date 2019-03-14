b6aa517d5b1963b2d1f7bd3f77bfea03dae526fa
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-14 13:50:21 +0100

    done zjump

--- projects/vm/srcs/opcode/zjump.c
+++ projects/vm/srcs/opcode/zjump.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 11:49:37 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/14 11:54:51 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/14 12:16:13 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,11 @@
 
 void exec_zjump(t_vm *vm, t_process *process, const t_op *op)
 {
+	uint16_t move_to;
+	uint16_t pos;
+
+	pos = get_idx_in_memory(process) - 1;
+	move_to = read_arg(process, vm->memory, T_DIR);
+	ft_printf("%hx %hx %hx\n", pos, move_to, pos + move_to % MEM_SIZE);
+	process_set_cursor_pos(process, pos + move_to);
 }
