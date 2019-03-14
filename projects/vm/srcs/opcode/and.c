7b7482fd646201715caa8002aaed9ae298774cf8
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-14 14:07:20 +0100

    correct and

--- projects/vm/srcs/opcode/and.c
+++ projects/vm/srcs/opcode/and.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 11:44:00 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/14 11:55:01 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/14 14:06:02 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,13 +58,14 @@ void exec_and(t_vm *vm, t_process *process, const t_op *op)
 	oc = read_octet_code(process, vm->memory);
 	ft_printf("type: %d %d %d\n", get_type_arg(oc, 0), get_type_arg(oc, 1),
 		get_type_arg(oc, 2));
-	if (read_and_arg(vm->memory, process, args, oc) == -1 ||
-		(logic = args[0] & args[1]) == 0)
+	if (read_and_arg(vm->memory, process, args, oc) == -1)
 	{
 		process->carry = 1;
 	}
 	else
 	{
+		logic = args[0] & args[1];
+		process->carry = logic == 0;
 		write_in_registre(process, args[2], logic);
 	}
 	ft_printf("args[0]: %u\n", args[0]);
