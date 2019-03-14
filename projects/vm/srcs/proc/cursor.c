48195237db3262d9c0999a6986bfd7d30eb9f1ad
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-14 13:48:45 +0100

    create function to set cursor

--- projects/vm/srcs/proc/cursor.c
+++ projects/vm/srcs/proc/cursor.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 15:34:09 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/12 15:36:42 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/14 12:02:07 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,9 @@
 void	process_move_cursor(t_process *process, off_t off)
 {
 	process->cursor_pos = (process->cursor_pos + off) % MEM_SIZE;
+}
+
+void	process_set_cursor_pos(t_process *process, size_t pos)
+{
+	process->cursor_pos = pos % MEM_SIZE;
 }
