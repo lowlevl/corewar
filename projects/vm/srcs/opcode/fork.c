bfa7c5c2854dd2cc00e05b9d53fed60e3b0af4de
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-15 12:56:41 +0100

    add mod mesize

--- projects/vm/srcs/opcode/fork.c
+++ projects/vm/srcs/opcode/fork.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 16:57:48 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/14 17:01:29 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/15 12:52:06 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void exec_fork(t_vm *vm, t_process *process, const t_op *op)
 	(void)op;
 	pos = get_idx_in_memory(process) - 1;
 	adr = read_arg(process, vm->memory, T_DIR);
-	copy_process(vm, process, pos + adr % IDX_MOD);
+	copy_process(vm, process, pos + adr % IDX_MOD % MEM_SIZE);
 }
