f69b3b7f568986aaacc38058afacbf453741cac2
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-25 10:45:30 +0100

    add debug print

--- projects/vm/srcs/opcode/zjump.c
+++ projects/vm/srcs/opcode/zjump.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 11:49:37 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/15 13:12:17 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/25 09:14:41 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,10 @@ void exec_zjump(t_vm *vm, t_process *process, const t_op *op)
 	(void)op;
 	pos = get_idx_in_memory(process) - 1;
 	move_to = read_arg(process, vm->memory, T_DIR);
-	ft_printf("%hx %hx %hx\n", pos, move_to, pos + move_to % MEM_SIZE);
 	if (process->carry == 1)
 	{
+		DEBUG_R_FC &&ft_dprintf(
+			2, "zjmp :(%hx + %hx = %hx)\n", pos, move_to, (pos + move_to) % MEM_SIZE);
 		process_set_cursor_pos(process, pos + move_to);
 	}
 }
