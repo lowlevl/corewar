0a0a173e95454e7211ae6528cb26af55e38a13ed
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-27 10:06:14 +0100

    desable flags

--- projects/vm/incs/flags.h
+++ projects/vm/incs/flags.h
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 16:20:03 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/22 16:23:58 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/03/27 09:51:20 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@
 ** 1 for on
 */
 
-# define DEBUG_READ 1
-# define DEBUG_WRITE 1
-# define DEBUG_GET 1
+# define DEBUG_READ 0
+# define DEBUG_WRITE 0
+# define DEBUG_GET 0
 # define DEBUG_TYPE 0
-# define DEBUG_R_FC 1
-# define DEBUG_CR_P 1
+# define DEBUG_R_FC 0
+# define DEBUG_CR_P 0
 # define DEBUG_EXE 0
 # define DEBUG_CYCLE 1
 # define DEBUG_TYPE 0
-# define DEBUG_CARRY 1
+# define DEBUG_CARRY 0
 
 #endif // !FALGS_H
