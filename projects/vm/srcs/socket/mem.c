c67f4d3f8d9a7e00af42d74e702137ae97d1f9af
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-03-29 16:34:05 +0100

    wip send mem

--- projects/vm/srcs/socket/mem.c
+++ projects/vm/srcs/socket/mem.c
@@ -1,32 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   live.c                                             :+:      :+:    :+:   */
+/*   mem.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/29 15:33:16 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/29 16:19:43 by fbenneto         ###   ########.fr       */
+/*   Created: 2019/03/29 16:23:19 by fbenneto          #+#    #+#             */
+/*   Updated: 2019/03/29 16:27:27 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "socket.h"
 
-#define FORMAT_LIVE "<live>%d</live>"
-
-int send_live(t_socket *socket, int32_t player_id)
-{
-	size_t len;
-	char *s;
-
-	if (socket->enable != ENABLE_SOCKET)
-		return 0;
-	len = ft_asprintf(&s, FORMAT_LIVE, player_id);
-	if (s && len > 0)
-	{
-		// DEBUG_SOCKET_SEND &&ft_dprintf(2, SOCKET_SEND, len, s);
-		send_message_to_all(socket, s, len);
-		free(s);
-	}
-	return 0;
-}
+#define FORMAT_MEM "<mem><idx>%zu</idx><size>%zu</size>%s</mem>"
--- projects/vm/srcs/socket/player_map.c
+++ projects/vm/srcs/socket/player_map.c
@@ -1,32 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   live.c                                             :+:      :+:    :+:   */
+/*   player_map.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/29 15:33:16 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/03/29 16:19:43 by fbenneto         ###   ########.fr       */
+/*   Created: 2019/03/29 16:23:09 by fbenneto          #+#    #+#             */
+/*   Updated: 2019/03/29 16:28:40 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "socket.h"
 
-#define FORMAT_LIVE "<live>%d</live>"
-
-int send_live(t_socket *socket, int32_t player_id)
-{
-	size_t len;
-	char *s;
-
-	if (socket->enable != ENABLE_SOCKET)
-		return 0;
-	len = ft_asprintf(&s, FORMAT_LIVE, player_id);
-	if (s && len > 0)
-	{
-		// DEBUG_SOCKET_SEND &&ft_dprintf(2, SOCKET_SEND, len, s);
-		send_message_to_all(socket, s, len);
-		free(s);
-	}
-	return 0;
-}
+#define FORMAT_MAP "<map><idx>%zu</idx><size>%zu</size>%s</map>"
