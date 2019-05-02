4bb4df7f790bd1c2aad187dd69e5c156865c9076
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-05-02 14:51:45 +0200

    add get flags

--- projects/vm/incs/flags.h
+++ projects/vm/incs/flags.h
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/21 16:20:03 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/05/02 13:58:37 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/05/02 14:29:17 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@
 # define DEBUG_VM 0
 # define DEBUG_READ  1
 # define DEBUG_WRITE 1
-# define DEBUG_GET   0
+# define DEBUG_GET   1
 # define DEBUG_TYPE  0
 # define DEBUG_R_FC  1
 # define DEBUG_CR_P  1
