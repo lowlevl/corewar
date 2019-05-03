bb0fcc7f03b9bac8b47b55fa79daf52b81b95598
FirelightFlagboy <firelight.flagboy@gmail.com>
2019-05-03 11:49:47 +0200

    encode player name in base 64

--- projects/vm/srcs/socket/player.c
+++ projects/vm/srcs/socket/player.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 11:53:07 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/04/16 13:56:55 by glodi            ###   ########.fr       */
+/*   Updated: 2019/05/03 11:49:25 by fbenneto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,16 @@
 #define FUU2 "<winner noWinner=\"false\"> <id>%d</id> <name>%s</name> </winner>"
 #define FORMAT_WINNER HEADER_SOCKET FUU2
 
-static void	do_stuff(t_player *player, t_socket *socket)
+static void do_stuff(t_player *player, t_socket *socket)
 {
-	char	*s;
-	int32_t	len;
+	char *		   s;
+	unsigned char *encoded_name __attribute__((cleanup(clean_simple_ptr)));
+	size_t		   olen;
+	int32_t		   len;
 
-	len = ft_asprintf(&s, FORMAT_PLAYER, player->id, player->header.prog_name);
+	encoded_name = encode_base64(
+		player->header.prog_name, ft_strlen(player->header.prog_name), &olen);
+	len = ft_asprintf(&s, FORMAT_PLAYER, player->id, encoded_name);
 	if (s && len > 0)
 	{
 		ft_memcpy(s, &len, sizeof(len));
@@ -32,9 +36,9 @@ static void	do_stuff(t_player *player, t_socket *socket)
 	}
 }
 
-int			send_players(t_vm *vm)
+int send_players(t_vm *vm)
 {
-	uint8_t	i;
+	uint8_t i;
 
 	if (vm->socket.enable != ENABLE_SOCKET)
 		return (0);
@@ -47,14 +51,19 @@ int			send_players(t_vm *vm)
 	return (0);
 }
 
-int			send_winner(t_player *player, t_socket *socket)
+int send_winner(t_player *player, t_socket *socket)
 {
-	char	*s;
-	int32_t	len;
+	char *		   s;
+	unsigned char *encoded_name __attribute__((cleanup(clean_simple_ptr)));
+	size_t		   olen;
+	int32_t		   len;
 
+	encoded_name = NULL;
 	if (socket->enable != ENABLE_SOCKET)
 		return (0);
-	len = ft_asprintf(&s, FORMAT_WINNER, player->id, player->header.prog_name);
+	encoded_name = encode_base64(
+		player->header.prog_name, ft_strlen(player->header.prog_name), &olen);
+	len = ft_asprintf(&s, FORMAT_WINNER, player->id, encoded_name);
 	if (s && len > 0)
 	{
 		ft_memcpy(s, &len, sizeof(len));
