9bbb8574d52eb3f94f8b8a4a1a03704eaaa9226a
Guillaume Lodi <guillaume.lodi@gmail.com>
2019-05-14 13:33:12 +0200

    Norm opcode/other.u.c

--- projects/vm/srcs/opcode/other.u.c
+++ projects/vm/srcs/opcode/other.u.c
@@ -6,35 +6,31 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/13 16:58:40 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/05/02 16:19:07 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/05/14 13:32:47 by glodi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "opcode.h"
 
-__attribute__((const)) uint16_t get_restrict_address(
-	int16_t current, int16_t plus)
+uint16_t	get_restrict_address(int16_t current, int16_t plus)
 {
-	// if (current + plus < 0)
-	// 	return (uint16_t)((current + plus % IDX_MOD + MEM_SIZE) % MEM_SIZE);
-	// return (uint16_t)((current + plus % IDX_MOD) % MEM_SIZE);
 	return (get_address(current, plus % IDX_MOD));
 }
 
-__attribute__((const)) uint16_t get_address(int16_t current, int16_t plus)
+uint16_t	get_address(int16_t current, int16_t plus)
 {
-	int16_t val;
+	int16_t	val;
 
 	val = current + plus;
 	if (val < 0)
 		val += MEM_SIZE;
 	val %= MEM_SIZE;
-	DEBUG_ADR &&ft_dprintf(2, ADR_PRE "test %hd + %hd = %hd ret (%hd)\n",
+	DEBUG_ADR && ft_dprintf(2, ADR_PRE "test %hd + %hd = %hd ret (%hd)\n",
 		current, plus, current + plus, val);
 	return (val);
 }
 
-int get_argument_all_restrict_4(
+int			get_argument_all_restrict_4(
 	size_t info[2], uint32_t *save, uint8_t *mem, t_process *proc)
 {
 	if (info[0] == T_REG)
@@ -48,7 +44,7 @@ int get_argument_all_restrict_4(
 	return (0);
 }
 
-int get_argument_all_restrict(
+int			get_argument_all_restrict(
 	size_t info[2], uint32_t *save, uint8_t *mem, t_process *proc)
 {
 	if (info[0] == T_REG)
@@ -62,7 +58,7 @@ int get_argument_all_restrict(
 	return (0);
 }
 
-int get_argument_all(
+int			get_argument_all(
 	size_t info[2], uint32_t *save, uint8_t *mem, t_process *proc)
 {
 	if (info[0] == T_REG)
