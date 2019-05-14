8a23735ed9d1c612d5662b9fd49243e6548a6dd3
Guillaume Lodi <guillaume.lodi@gmail.com>
2019-05-14 13:20:27 +0200

    Norm dump/print.c

--- projects/vm/srcs/dump/print.c
+++ projects/vm/srcs/dump/print.c
@@ -6,13 +6,13 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/06 14:47:04 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/05/03 14:27:27 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/05/14 13:20:10 by glodi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <dump.h>
 
-static void add_hex_to_line(char *line, uint8_t val)
+static void			add_hex_to_line(char *line, uint8_t val)
 {
 	line[0] = (val >> 4);
 	line[1] = (val & 0xf);
@@ -27,7 +27,7 @@ static void add_hex_to_line(char *line, uint8_t val)
 	line[2] = ' ';
 }
 
-static inline int print_color(uint8_t val)
+static inline int	print_color(uint8_t val)
 {
 	if (val == 0)
 		return (ft_printf("\e[0m"));
@@ -42,12 +42,12 @@ static inline int print_color(uint8_t val)
 	return (0);
 }
 
-static inline void routine_col(uint8_t *mem, uint8_t *heat_map, off_t off)
+static inline void	routine_col(uint8_t *mem, uint8_t *heat_map, off_t off)
 {
-	int	c;
-	int	idx;
-	char   line[BYTE_PRINT_LENGTH + 1];
-	int8_t lastheat;
+	int		c;
+	int		idx;
+	char	line[BYTE_PRINT_LENGTH + 1];
+	int8_t	lastheat;
 
 	c = 0;
 	lastheat = -1;
@@ -66,9 +66,8 @@ static inline void routine_col(uint8_t *mem, uint8_t *heat_map, off_t off)
 	}
 }
 
-static inline void routine_line(uint8_t *mem, uint8_t *heat_map, int l)
+static inline void	routine_line(uint8_t *mem, uint8_t *heat_map, int l)
 {
-
 	while (l < NB_LINE)
 	{
 		ft_printf("0x%.4x: ", l * BYTE_PER_LINE);
@@ -78,13 +77,13 @@ static inline void routine_line(uint8_t *mem, uint8_t *heat_map, int l)
 	}
 }
 
-int print_memory(uint8_t *mem, uint8_t *heat_map)
+int					print_memory(uint8_t *mem, uint8_t *heat_map)
 {
 	ft_printf("index : 00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f"
-			  " 10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f"
-			  " 20 21 22 23 22 25 26 27 28 29 2a 2b 2c 2d 2e 2f"
-			  " 30 31 32 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f"
-			  "\n");
+			" 10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f"
+			" 20 21 22 23 22 25 26 27 28 29 2a 2b 2c 2d 2e 2f"
+			" 30 31 32 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f"
+			"\n");
 	routine_line(mem, heat_map, 0);
 	return (0);
 }
