9283984e4481d0eb851cf0b1346c3804252d77eb
Guillaume Lodi <guillaume.lodi@gmail.com>
2019-05-14 11:56:45 +0200

    Norm player.c

--- projects/vm/srcs/socket/player.c
+++ projects/vm/srcs/socket/player.c
@@ -6,7 +6,7 @@
 /*   By: fbenneto <fbenneto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 11:53:07 by fbenneto          #+#    #+#             */
-/*   Updated: 2019/05/03 12:00:07 by fbenneto         ###   ########.fr       */
+/*   Updated: 2019/05/14 11:56:37 by glodi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,16 @@
 
 #define FUCK_1 "<player> <id>%d</id> <size>%d</size> <name>%s</name> </player>"
 #define FORMAT_PLAYER HEADER_SOCKET FUCK_1
-#define FUU2 "<winner noWinner=\"false\"> <id>%d</id> <size>%d</size> <name>%s</name> </winner>"
-#define FORMAT_WINNER HEADER_SOCKET FUU2
+#define FUU2 "<winner noWinner=\"false\"> <id>%d</id> <size>%d</size> "
+#define FUU3 "<name>%s</name> </winner>"
+#define FORMAT_WINNER HEADER_SOCKET FUU2 FUU3
 
-static void do_stuff(t_player *player, t_socket *socket)
+static void	do_stuff(t_player *player, t_socket *socket)
 {
-	char *		   s;
-	unsigned char *encoded_name __attribute__((cleanup(clean_simple_ptr)));
-	size_t		   olen;
-	int32_t		   len;
+	char			*s;
+	unsigned char	*encoded_name __attribute__((cleanup(clean_simple_ptr)));
+	size_t			olen;
+	int32_t			len;
 
 	encoded_name = encode_base64(
 		player->header.prog_name, ft_strlen(player->header.prog_name), &olen);
@@ -36,9 +37,9 @@ static void do_stuff(t_player *player, t_socket *socket)
 	}
 }
 
-int send_players(t_vm *vm)
+int			send_players(t_vm *vm)
 {
-	uint8_t i;
+	uint8_t	i;
 
 	if (vm->socket.enable != ENABLE_SOCKET)
 		return (0);
@@ -51,12 +52,12 @@ int send_players(t_vm *vm)
 	return (0);
 }
 
-int send_winner(t_player *player, t_socket *socket)
+int			send_winner(t_player *player, t_socket *socket)
 {
-	char *		   s;
-	unsigned char *encoded_name __attribute__((cleanup(clean_simple_ptr)));
-	size_t		   olen;
-	int32_t		   len;
+	char			*s;
+	unsigned char	*encoded_name __attribute__((cleanup(clean_simple_ptr)));
+	size_t			olen;
+	int32_t			len;
 
 	encoded_name = NULL;
 	if (socket->enable != ENABLE_SOCKET)
